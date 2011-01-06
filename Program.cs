a423bc95f141684b25271942af679044561254fe
dzamkov <dzamkov@xiatek.org>
2011-01-05 22:14:55 -0500

    Complex numbers, fft and inverse

--- Program.cs
+++ Program.cs
@@ -41,21 +41,42 @@ namespace MD
 
             int s = AL.GenSource();
 
-            ALFormat format = mstr.Format == SoundFormat.Pcm16BitMono ? ALFormat.Mono16 : ALFormat.Stereo16;
             bool playing = false;
-            byte[] data = new byte[50000];
+            byte[] data = new byte[65536];
+            Complex[] cdata = new Complex[data.Length / 4];
+            Complex[] ndata = new Complex[data.Length / 4];
             while (true)
             {
 
                 int amount = mstr.Read(data, 0, data.Length);
                 Console.WriteLine("WROTE: " + amount.ToString());
+
+                for(int t = 0; t < amount / 4; t++)
+                {
+                    cdata[t] = (double)data[t * 4 + 1] / 256.0;
+                }
+
+                // Fourier transform
+                if (amount == data.Length)
+                {
+                    FFT.OnArray(false, cdata, ndata, cdata.Length);
+                    FFT.OnArray(true, ndata, cdata, cdata.Length);
+                }
+
+                // Data fix
+                for (int t = 0; t < cdata.Length; t++)
+                {
+                    byte h = (byte)(cdata[t].Real * 255.0);
+                    data[t * 2 + 1] = h;
+                }
+
                 if (mstr.Position == mstr.Length)
                 {
                     break;
                 }
 
                 int buf = AL.GenBuffer();
-                AL.BufferData<byte>(buf, format, data, 50000, mstr.Frequency);
+                AL.BufferData<byte>(buf, ALFormat.Mono16, data, data.Length / 2, mstr.Frequency);
                 AL.SourceQueueBuffer(s, buf);
 
                 if (!playing)
